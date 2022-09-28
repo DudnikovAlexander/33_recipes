c8c5112a2134dc33fe90d258f278a77e20a7406d
Dudnikov ALexander <ADudnikov@ibs.ru>
2022-09-28 16:29:14 +0400

    Добавил  recipes_tag.html left_nav.html

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
