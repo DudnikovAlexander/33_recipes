7d3420c4bdb48bfbbbadb9a4c3e60f789e3e8468
Dudnikov ALexander <ADudnikov@ibs.ru>
2022-09-28 15:09:04 +0400

    Добавил \templates\blog\home.html, установил django-ckditor

--- config/settings.py
+++ config/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'debug_toolbar',
     'mptt',
+    'ckeditor',
 
     'blog',
+    'contact',
+    'gallery',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +136,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
